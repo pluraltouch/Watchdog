2721cb8c895634e22f8dd85c0d6beb8fa4dd547f
gabor <gabor.horovitz@gmail.com>
2021-01-26 07:13:12 +0100

    Allow console applications to show up

--- Server/WatchdogLib/HeartbeatServer.cs
+++ Server/WatchdogLib/HeartbeatServer.cs
@@ -10,38 +10,44 @@ namespace WatchdogLib
     {
         public HeartbeatClient(string name)
         {
-            Name        = name;
+            Name = name;
             RequestKill = false;
             LastHeartbeat = DateTime.Now;
             LogManager.GetLogger("WatchdogServer");
-
         }
-        public string Name            { get; set; }
-        public string ProcessName     { get; set; }
+
+        public string Name { get; set; }
+        public string ProcessName { get; set; }
         public DateTime LastHeartbeat { get; set; }
-        public bool RequestKill       { get; set; }
-        public DateTime KillTime      { get; set; }
+        public bool RequestKill { get; set; }
+        public DateTime KillTime { get; set; }
     }
 
     public class HeartbeatServer
     {
-        private enum Commands
-        {
-            SetTimeOut = 0,
-            Heartbeat = 1,
-            RequestKill = 2,
-        }
-
         private const string PipeName = "named_pipe_watchdog";
-        private readonly NamedPipeServer<string> _server  = new NamedPipeServer<string>(PipeName);
-        private readonly ISet<HeartbeatClient>   _clients = new HashSet<HeartbeatClient>();
-        private readonly DateTime _serverStarted;
-
-     //   public uint HardTimeout                 { get; set; }
-       // public uint SoftTimeout                 { get; set; }
-        public ISet<HeartbeatClient> Clients    { get { return _clients; } }
 
         private static HeartbeatServer _instance;
+        private readonly NamedPipeServer<string> _server = new NamedPipeServer<string>(PipeName);
+        private readonly ISet<HeartbeatClient> _clients = new HashSet<HeartbeatClient>();
+        private readonly DateTime _serverStarted;
+
+        private HeartbeatServer()
+        {
+            Logger = LogManager.GetLogger("WatchdogServer");
+            _server.ClientConnected += OnClientConnected;
+            _server.ClientDisconnected += OnClientDisconnected;
+            _server.ClientMessage += OnClientMessage;
+            //_serverStarted              = DateTime.Now;
+            //HardTimeout                 = hardTimeout;
+            //SoftTimeout                 = softTimeout;
+            _serverStarted = DateTime.Now;
+            _server.Start();
+        }
+
+        //   public uint HardTimeout                 { get; set; }
+        // public uint SoftTimeout                 { get; set; }
+        public ISet<HeartbeatClient> Clients => _clients;
 
         public static HeartbeatServer Instance
         {
@@ -51,101 +57,26 @@ namespace WatchdogLib
                 {
                     _instance = new HeartbeatServer();
                 }
+
                 return _instance;
             }
         }
 
         public Logger Logger { get; set; }
 
-        private HeartbeatServer()
-        {
-            Logger                      = LogManager.GetLogger("WatchdogServer");
-            _server.ClientConnected    += OnClientConnected;
-            _server.ClientDisconnected += OnClientDisconnected;
-            _server.ClientMessage      += OnClientMessage;
-            //_serverStarted              = DateTime.Now;
-            //HardTimeout                 = hardTimeout;
-            //SoftTimeout                 = softTimeout;
-            _serverStarted                = DateTime.Now;
-            _server.Start();                        
-        }        
-
-        private void OnClientMessage(NamedPipeConnection<string, string> connection, string message)
-        {
-            var args = message.Split(',');
-            if (args.Length == 0) return;
-            uint command; if (!uint.TryParse(args[0], out command)) return;
-
-            switch (command)
-            {
-
-                case (int)Commands.Heartbeat:
-                    {
-                        var client = FindByName(connection.Name);
-                        if (client==null) break;
-                        if (args.Length < 2) return;
-                        client.ProcessName = args[1];
-                        client.LastHeartbeat = DateTime.Now;
-                        Debug.WriteLine("received heartbeat");
-                    }
-                    break;
-                case (int)Commands.RequestKill:
-                    {
-                        var client = FindByName(connection.Name);
-                        if (client == null) break;
-                        if (args.Length < 2) return;
-                        client.ProcessName = args[1];
-                        
-                        if (args.Length == 3)
-                        {
-                            uint delay;
-                            if (!uint.TryParse(args[2], out delay)) return;
-                            client.KillTime = DateTime.Now + TimeSpan.FromSeconds(delay);
-                            Logger.Warn("Received kill after {0} seconds request by Process {1}", delay, client.ProcessName);
-                            //Debug.WriteLine("received delayed kill");
-                        }
-                        else
-                        {
-                            client.KillTime = DateTime.Now ;
-                            Logger.Warn("Received kill request by Process {0}", client.ProcessName);
-                            //Debug.WriteLine("received  kill");
-                        }
-                        client.RequestKill = true;
-                    }
-                    break;
-                default:
-                    Debug.WriteLine("Unrecognized command");
-                    break;
-            }
-        }
-
-        private void OnClientConnected(NamedPipeConnection<string, string> connection)
-        {
-            if (FindByName(connection.Name) == null)
-            {                
-                _clients.Add(new HeartbeatClient(connection.Name));
-            }
-            //SendCommand(Commands.SetTimeOut, HardTimeout);
-        }
-
-        private void OnClientDisconnected(NamedPipeConnection<string, string> connection)
-        {
-            var client = FindByName(connection.Name);
-            if (client != null) _clients.Remove(client);
-        }
-
         public HeartbeatClient FindByName(string name)
         {
-            return _clients.FirstOrDefault((client) => client.Name == name);
+            return _clients.FirstOrDefault(client => client.Name == name);
         }
 
         public HeartbeatClient FindByProcessName(string processName)
         {
-            return _clients.FirstOrDefault((client) => client.ProcessName == processName);
+            return _clients.FirstOrDefault(client => client.ProcessName == processName);
         }
+
         /// <summary>
-        /// Returns true if the last heartbeat was longer ago than the timeout. 
-        /// Note that if the process is not connected, the function returns false.
+        ///     Returns true if the last heartbeat was longer ago than the timeout.
+        ///     Note that if the process is not connected, the function returns false.
         /// </summary>
         /// <param name="processName">Name of process</param>
         /// <returns></returns>
@@ -175,7 +106,7 @@ namespace WatchdogLib
         //public bool HeartbeatHardTimeoutOrDisconnect(string processName)
         //{          
         //    return HeartbeatHardTimeout(processName) || !Connected(processName);
-            
+
         //}
 
         ///// <summary>
@@ -211,8 +142,6 @@ namespace WatchdogLib
         //{
         //    return HeartbeatSoftTimeout(processName) || !Connected(processName);
         //}
-
-
         public bool HeartbeatTimedOutOrDisconnect(string processName, uint timeout)
         {
             return HeartbeatTimedOut(processName, timeout) || !Connected(processName);
@@ -227,19 +156,15 @@ namespace WatchdogLib
                 // No process with this name connected, so no timeout
                 return false;
             }
-            else
+
+            if ((DateTime.Now - _serverStarted).TotalSeconds < 2 * timeout)
             {
-                if ((DateTime.Now - _serverStarted).TotalSeconds < 2 * timeout)
-                {
-                    // Server is not running long enough to have reliably received a heartbeat
-                    return false;
-                }
-                else
-                {
-                    // Check if last heartbeat was before timeout
-                    return ((DateTime.Now - client.LastHeartbeat).TotalSeconds > timeout);
-                }
+                // Server is not running long enough to have reliably received a heartbeat
+                return false;
             }
+
+            // Check if last heartbeat was before timeout
+            return ((DateTime.Now - client.LastHeartbeat).TotalSeconds > timeout);
         }
 
         public bool Connected(string processName)
@@ -251,10 +176,115 @@ namespace WatchdogLib
         public bool KillRequested(string processName)
         {
             var client = FindByProcessName(processName);
-            if (client == null) return false;
-            var performKill =  client.RequestKill && (DateTime.Now > client.KillTime );
-            if (performKill ) client.RequestKill = false; // Kill request only returns true once
+            if (client == null)
+            {
+                return false;
+            }
+
+            var performKill = client.RequestKill && (DateTime.Now > client.KillTime);
+            if (performKill)
+            {
+                client.RequestKill = false; // Kill request only returns true once
+            }
+
             return performKill;
+        }
+
+        private void OnClientMessage(NamedPipeConnection<string, string> connection, string message)
+        {
+            var args = message.Split(',');
+            if (args.Length == 0)
+            {
+                return;
+            }
+
+            uint command;
+            if (!uint.TryParse(args[0], out command))
+            {
+                return;
+            }
+
+            switch (command)
+            {
+                case (int) Commands.Heartbeat:
+                {
+                    var client = FindByName(connection.Name);
+                    if (client == null)
+                    {
+                        break;
+                    }
+
+                    if (args.Length < 2)
+                    {
+                        return;
+                    }
+
+                    client.ProcessName = args[1];
+                    client.LastHeartbeat = DateTime.Now;
+                    Debug.WriteLine("received heartbeat");
+                }
+                    break;
+                case (int) Commands.RequestKill:
+                {
+                    var client = FindByName(connection.Name);
+                    if (client == null)
+                    {
+                        break;
+                    }
+
+                    if (args.Length < 2)
+                    {
+                        return;
+                    }
+
+                    client.ProcessName = args[1];
+
+                    if (args.Length == 3)
+                    {
+                        uint delay;
+                        if (!uint.TryParse(args[2], out delay))
+                        {
+                            return;
+                        }
+
+                        client.KillTime = DateTime.Now + TimeSpan.FromSeconds(delay);
+                        Logger.Warn("Received kill after {0} seconds request by Process {1}", delay,
+                            client.ProcessName);
+                        //Debug.WriteLine("received delayed kill");
+                    }
+                    else
+                    {
+                        client.KillTime = DateTime.Now;
+                        Logger.Warn("Received kill request by Process {0}", client.ProcessName);
+                        //Debug.WriteLine("received  kill");
+                    }
+
+                    client.RequestKill = true;
+                }
+                    break;
+                default:
+                    Debug.WriteLine("Unrecognized command");
+                    break;
+            }
+        }
+
+        private void OnClientConnected(NamedPipeConnection<string, string> connection)
+        {
+            if (FindByName(connection.Name) == null)
+            {
+                _clients.Add(new HeartbeatClient(connection.Name));
+            }
+
+            //SendCommand(Commands.SetTimeOut, HardTimeout);
+        }
+
+        private void OnClientDisconnected(NamedPipeConnection<string, string> connection)
+        {
+            var client = FindByName(connection.Name);
+            if (client != null)
+            {
+                _clients.Remove(client);
+            }
         }
 
         //private void SendCommand(Commands command, string[] arguments)
@@ -269,7 +299,14 @@ namespace WatchdogLib
         //}
         private void SendCommand<T>(Commands command, T argument)
         {
-            _server.PushMessage(((int)command).ToString() + "," + argument.ToString());
+            _server.PushMessage(((int) command) + "," + argument);
+        }
+
+        private enum Commands
+        {
+            SetTimeOut = 0,
+            Heartbeat = 1,
+            RequestKill = 2
         }
 
         //private void SendCommand<T1, T2>(Commands command, T1 argument1, T2 argument2)
--- Server/WatchdogLib/ProcessHander.cs
+++ Server/WatchdogLib/ProcessHander.cs
@@ -1,78 +1,82 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using Utilities;
 
 namespace WatchdogLib
 {
-
     // todo: http://msdn.microsoft.com/en-us/library/system.diagnostics.process.exitcode(v=vs.110).aspx
     //http://stackoverflow.com/questions/2279181/catch-another-process-unhandled-exception
     // http://social.msdn.microsoft.com/Forums/vstudio/en-US/62259e21-3280-4d10-a27c-740d35efe51c/catch-another-process-unhandled-exception?forum=csharpgeneral
     public class ProgressEventArgs : EventArgs
     {
-        public float Progress { get; private set; }
-        public Process Process { get; private set; }
         public ProgressEventArgs(float progress, Process process)
         {
             Progress = progress;
             Process = process;
         }
+
+        public float Progress { get; }
+        public Process Process { get; }
     }
 
     public class ProcessMessageArgs : EventArgs
     {
-        public string Message { get; private set; }
-        public Process Process { get; private set; }
         public ProcessMessageArgs(string message, Process process)
         {
             Message = message;
             Process = process;
         }
+
+        public string Message { get; }
+        public Process Process { get; }
     }
 
     public class ProcessStatusArgs : EventArgs
     {
-        public int ExitCode { get; private set; }
-        public Process Process { get; private set; }
         public ProcessStatusArgs(int exitCode, Process process)
         {
             ExitCode = exitCode;
             Process = process;
         }
+
+        public int ExitCode { get; }
+        public Process Process { get; }
     }
 
     public class ProcessHandler
     {
-
         private readonly object _exitedLock = new object();
-        private Stopwatch _nonresponsiveInterval;
-        private Stopwatch _fromStart;
+        private readonly Stopwatch _nonresponsiveInterval;
+        private readonly Stopwatch _fromStart;
 
         public DataReceivedEventHandler OutputHandler;
         public EventHandler<ProcessMessageArgs> ErrorOutputHandler;
-        
-        public event EventHandler<ProcessStatusArgs> ExitHandler;
-        public event EventHandler<ProcessMessageArgs> ErrorHandler;
 
 
- 
-        public int NonResponsiveInterval  { get; set; }
-        public string Executable          { get; set; }
+        public ProcessHandler()
+        {
+            WaitForExit = true;
+            RunInDir = true;
+            NonresponsiveInterval = 2000;
+            StartingInterval = 5000;
+            _nonresponsiveInterval = new Stopwatch();
+            _fromStart = new Stopwatch();
+        }
 
-        public string Args                { get; set; }
 
-        public bool WaitForExit           { get; set; }
-        public bool RunInDir              { get; set; }
+        public int NonResponsiveInterval { get; set; }
+        public string Executable { get; set; }
+
+        public string Args { get; set; }
+
+        public bool WaitForExit { get; set; }
+        public bool RunInDir { get; set; }
         public uint NonresponsiveInterval { get; set; }
 
-        public uint StartingInterval      { get; set; }
+        public uint StartingInterval { get; set; }
 
-        public bool HasExited
-        {
-            get { return (Process == null) || Process.HasExited; }
-        }
+        public bool HasExited => (Process == null) || Process.HasExited;
 
         public bool Responding
         {
@@ -81,7 +85,10 @@ namespace WatchdogLib
                 // todo: add heartbeat
                 if (!Process.Responding)
                 {
-                    if (!_nonresponsiveInterval.IsRunning) _nonresponsiveInterval.Restart();
+                    if (!_nonresponsiveInterval.IsRunning)
+                    {
+                        _nonresponsiveInterval.Restart();
+                    }
                 }
                 else
                 {
@@ -92,16 +99,16 @@ namespace WatchdogLib
             }
         }
 
+        public bool Running { get; set; }
 
-        public ProcessHandler()
-        {
-            WaitForExit            = true;
-            RunInDir               = true;
-            NonresponsiveInterval  = 2000;
-            StartingInterval       = 5000;
-            _nonresponsiveInterval = new Stopwatch();
-            _fromStart             = new Stopwatch();
-        }
+
+        public Process Process { get; private set; }
+        public string Name { get; private set; }
+
+        public bool NotRespondingAfterInterval =>
+            (!Responding && _nonresponsiveInterval.ElapsedMilliseconds > NonresponsiveInterval);
+
+        public bool IsStarting => (_fromStart.ElapsedMilliseconds < StartingInterval);
 
 
         public void CallExecutable(string executable, string args)
@@ -117,8 +124,8 @@ namespace WatchdogLib
             try
             {
                 Process.OutputDataReceived += Output;
-                Process.ErrorDataReceived  += OutputError;
-                Name                        = Process.ProcessName;
+                Process.ErrorDataReceived += OutputError;
+                Name = Process.ProcessName;
                 _fromStart.Restart();
                 if (WaitForExit)
                 {
@@ -132,55 +139,62 @@ namespace WatchdogLib
             }
             catch (Exception ex)
             {
-                if (ErrorHandler != null) ErrorHandler(this, new ProcessMessageArgs(ex.Message, Process));
-                if (Process.HasExited)
+                if (ErrorHandler != null)
                 {
-                    if (ExitHandler != null) ExitHandler(this, new ProcessStatusArgs(-1, Process));
-                    return false;
+                    ErrorHandler(this, new ProcessMessageArgs(ex.Message, Process));
                 }
 
-            }
-            return true;
-        }
+                if (Process.HasExited)
+                {
+                    if (ExitHandler != null)
+                    {
+                        ExitHandler(this, new ProcessStatusArgs(-1, Process));
+                    }
 
-        private void EndProcess()
-        {
-            if (Process==null) return;
-            Process.Close();
-            Process.Dispose();
-            Process = null;
+                    return false;
+                }
+            }
+
+            return true;
         }
 
 
         public void CallExecutable()
         {
-            
-            if (!File.Exists(Executable)) return;
+            if (!File.Exists(Executable))
+            {
+                return;
+            }
+
             var commandLine = Executable;
             Trace.WriteLine("Running command: " + Executable + " " + Args);
             var psi = new ProcessStartInfo(commandLine)
             {
                 UseShellExecute = false,
                 LoadUserProfile = false,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-                CreateNoWindow = true,
+                //RedirectStandardOutput = true,
+                //RedirectStandardError = true,
+                //WindowStyle = ProcessWindowStyle.Hidden,
+                //CreateNoWindow = true,
                 Arguments = Args
             };
             if (RunInDir)
             {
                 var path = Path.GetDirectoryName(Executable);
-                if (path != null) psi.WorkingDirectory = path;
+                if (path != null)
+                {
+                    psi.WorkingDirectory = path;
+                } 
             }
-            Process = new Process { StartInfo = psi };
+
+            Process = new Process {StartInfo = psi};
             try
-            {               
+            {
                 Process.Start();
-                Process.BeginOutputReadLine();
-                Process.BeginErrorReadLine();
-                Process.OutputDataReceived += Output;
-                Process.ErrorDataReceived += OutputError;
+                //Process.BeginOutputReadLine();
+                //Process.BeginErrorReadLine();
+                //Process.OutputDataReceived += Output;
+                //Process.ErrorDataReceived += OutputError;
                 _fromStart.Restart();
                 Name = Process.ProcessName;
 
@@ -197,34 +211,19 @@ namespace WatchdogLib
             }
             catch (Exception ex)
             {
-                if (ErrorHandler != null) ErrorHandler(this, new ProcessMessageArgs(ex.Message, Process));
-                if (!ProcessUtils.ProcessRunning(Executable))
+                if (ErrorHandler != null)
                 {
-                    if (ExitHandler != null) ExitHandler(this, new ProcessStatusArgs(-1, Process));
+                    ErrorHandler(this, new ProcessMessageArgs(ex.Message, Process));
                 }
 
+                if (!ProcessUtils.ProcessRunning(Executable))
+                {
+                    if (ExitHandler != null)
+                    {
+                        ExitHandler(this, new ProcessStatusArgs(-1, Process));
+                    }
+                }
             }
-        }
-
-        private void ProcessExited(object sender, EventArgs e)
-        {
-            Running = true;
-        }
-
-        public bool Running { get; set; }
-
-
-        public Process Process { get; private set; }
-        public string Name { get; private set; }
-
-        public bool NotRespondingAfterInterval
-        {
-            get { return (!Responding && _nonresponsiveInterval.ElapsedMilliseconds > NonresponsiveInterval); }
-        }
-
-        public bool IsStarting
-        {
-            get { return (_fromStart.ElapsedMilliseconds < StartingInterval); }
         }
 
 
@@ -233,30 +232,61 @@ namespace WatchdogLib
             EndProcess();
         }
 
-   
-        private void Output(object sender, DataReceivedEventArgs dataReceivedEventArgs)
-        {
-            if (string.IsNullOrEmpty(dataReceivedEventArgs.Data)) return;
-
-            var output = dataReceivedEventArgs.Data;
-            // Fire Output event
-            if (OutputHandler != null) OutputHandler(sender, dataReceivedEventArgs);
-        }
-
-        private void OutputError(object sender, DataReceivedEventArgs dataReceivedEventArgs)
-        {
-            if (string.IsNullOrEmpty(dataReceivedEventArgs.Data)) return;
-            // Fire OutputError event
-            var progressEventArgs = new ProcessMessageArgs(dataReceivedEventArgs.Data, Process);
-            if (ErrorOutputHandler != null) ErrorOutputHandler(sender, progressEventArgs);
-        }
-
         public bool Kill()
         {
             return ProcessUtils.KillProcess(Process);
             // Todo set state indicating that kill has been tried
         }
+
+        private void EndProcess()
+        {
+            if (Process == null)
+            {
+                return;
+            }
+
+            Process.Close();
+            Process.Dispose();
+            Process = null;
+        }
+
+        private void ProcessExited(object sender, EventArgs e)
+        {
+            Running = true;
+        }
+
+
+        private void Output(object sender, DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            if (string.IsNullOrEmpty(dataReceivedEventArgs.Data))
+            {
+                return;
+            }
+
+            var output = dataReceivedEventArgs.Data;
+            // Fire Output event
+            if (OutputHandler != null)
+            {
+                OutputHandler(sender, dataReceivedEventArgs);
+            }
+        }
+
+        private void OutputError(object sender, DataReceivedEventArgs dataReceivedEventArgs)
+        {
+            if (string.IsNullOrEmpty(dataReceivedEventArgs.Data))
+            {
+                return;
+            }
+
+            // Fire OutputError event
+            var progressEventArgs = new ProcessMessageArgs(dataReceivedEventArgs.Data, Process);
+            if (ErrorOutputHandler != null)
+            {
+                ErrorOutputHandler(sender, progressEventArgs);
+            }
+        }
+
+        public event EventHandler<ProcessStatusArgs> ExitHandler;
+        public event EventHandler<ProcessMessageArgs> ErrorHandler;
     }
-
-
 }
